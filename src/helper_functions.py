5825eeeb2cb6669de380a9e41cc4fae3e378ad13
willsc <cameron.wills.1998@gmail.com>
2024-06-14 12:58:00 -0600

    implemented argparse

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,11 +1,12 @@
 """
 Functions to create reports
 """
+import psycopg2 as pg2
 from psycopg2 import OperationalError
 import pandas as pd
 
 
-def create_report(con, sql_file, xlsx_out, xlsx_name):
+def create_report(con, sql_file, xlsx_out, xlsx_name=None):
     """
     Creates a report from a given .sql file and exports the data into a excel file
 
@@ -20,11 +21,26 @@ def create_report(con, sql_file, xlsx_out, xlsx_name):
     """
     try:
         with con:
+            if xlsx_name is None:
+                file_name = sql_file.split("/")[-1]
+                xlsx_name = file_name.split(".")[0]
             with open(sql_file, "r",
                       encoding="utf-8") as file:
                 query = file.read()
-                test = pd.read_sql_query(query, con)
-                test.to_excel(f"{xlsx_out}/{xlsx_name}.xlsx")
-        print(f"Created {xlsx_name} excel file!")
+                if query:
+                    test = pd.read_sql_query(query, con)
+                    test.to_excel(f"{xlsx_out}/{xlsx_name}.xlsx")
+                    print(f"Created {xlsx_name} excel file!")
     except OperationalError as e:
         print(e)
+
+
+def create_connection(db, usr, pw, host, port):
+    con = pg2.connect(
+        dbname=db,
+        user=usr,
+        password=pw,
+        host=host,
+        port=port
+    )
+    return con
--- src/main.py
+++ src/main.py
@@ -1,31 +1,33 @@
 """
 Generates reports
 """
-from os import getenv
+import os
 from dotenv import load_dotenv
 import psycopg2 as pg2
-from helper_functions import create_report
+import argparse
+from helper_functions import create_report, create_connection
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    PG_USER = getenv("PG_USER")
-    PG_PASSWORD = getenv("PG_PASSWORD")
-    PG_HOST = getenv("PG_HOST")
-    PG_PORT = getenv("PG_PORT")
-    PG_DB = getenv("PG_DB")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-sp", "--sql_path",
+                        help="Path to folder containing sql queries")
+    args = parser.parse_args()
 
-    con = pg2.connect(
-        dbname=PG_DB,
-        user=PG_USER,
-        password=PG_PASSWORD,
-        host=PG_HOST,
-        port=PG_PORT
-    )
-    print(type(con))
-    create_report(con=con,
-                  sql_file="sql_queries/prod_q3.sql",
-                  xlsx_out="excel_reports",
-                  xlsx_name="prod_report_3")
+    sql_dir = args.sql_path
+
+    load_dotenv()
+    PG_USER = os.getenv("PG_USER")
+    PG_PASSWORD = os.getenv("PG_PASSWORD")
+    PG_HOST = os.getenv("PG_HOST")
+    PG_PORT = os.getenv("PG_PORT")
+    PG_DB = os.getenv("PG_DB")
+
+    con = create_connection(PG_DB, PG_USER, PG_PASSWORD, PG_HOST, PG_PORT)
+
+    for ix, file in enumerate(os.listdir(sql_dir)):
+        create_report(con=con,
+                      sql_file=f"{sql_dir}/{file}",
+                      xlsx_out="excel_reports")
 
     con.close()
